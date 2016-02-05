c1b9705e152ed4d5bfba76bff9d1412b25984245
BHATTACHARYA, ARUNIMA <782328@ehs-s-itc-13>
2016-02-05 10:42:43 -0600

    Initial Commit. Basic game screen set up that increases and decreases balloon size.

---
+++ BalloonButton/Button.swift
@@ -0,0 +1,13 @@
+//
+//  Button.swift
+//  BalloonButton
+//
+//  Created by BHATTACHARYA, ARUNIMA on 2/2/16.
+//  Copyright © 2016 BHATTACHARYA, ARUNIMA. All rights reserved.
+//
+
+import SpriteKit
+
+class Button: SKSpriteNode {
+    
+}
--- BalloonButton/GameScene.swift
+++ BalloonButton/GameScene.swift
@@ -8,18 +8,34 @@
 
 import SpriteKit
 
+//let myButton1 = SKSpriteNode(imageNamed: "RedSqaure-1")
+
 class GameScene: SKScene {
+    let myButton1 = SKSpriteNode(imageNamed: "RedSqaure-1")
+    let myButton2 = SKSpriteNode(imageNamed: "RedSqaure-1")
+    let myBalloon = SKSpriteNode(imageNamed: "Circle")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        let myLabel = SKLabelNode(fontNamed: "Cochin-Bold")
+        myLabel.text = "Maintain Homeostasis"
+        myLabel.fontSize = 50
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame))
+        myBalloon.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        myButton1.position = CGPoint(x: CGRectGetMidX(self.frame), y: 150)
+        myButton1.xScale = 1.25
+        myButton1.yScale = 1.25
+        myButton2.position = CGPoint(x: CGRectGetMidX(self.frame), y: 600)
+        myButton2.xScale = 1.25
+        myButton2.yScale = 1.25
         
+        self.addChild(myButton1)
+        self.addChild(myButton2)
+        self.addChild(myBalloon)
         self.addChild(myLabel)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    /*override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
         for touch in touches {
@@ -36,6 +52,23 @@ class GameScene: SKScene {
             sprite.runAction(SKAction.repeatActionForever(action))
             
             self.addChild(sprite)
+            
+        }
+    }*/
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first!
+        if myButton1.containsPoint(touch.locationInNode(self)) {
+            let myBalloonHScale = myBalloon.xScale
+            let myBalloonVScale = myBalloon.yScale
+            myBalloon.xScale = myBalloonHScale * 1.025
+            myBalloon.yScale = myBalloonVScale * 1.025
+        }
+        if myButton2.containsPoint(touch.locationInNode(self)) {
+            let myBalloonHScale = myBalloon.xScale
+            let myBalloonVScale = myBalloon.yScale
+            myBalloon.xScale = myBalloonHScale * 0.975
+            myBalloon.yScale = myBalloonVScale * 0.975
         }
     }
    
