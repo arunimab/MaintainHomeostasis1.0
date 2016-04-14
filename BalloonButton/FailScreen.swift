a8ba4a7b875081444debe6cc3a782f1c0a4c7515
BHATTACHARYA, ARUNIMA <782328@ehs-s-itc-13>
2016-04-14 10:34:51 -0500

    Updated Code... Minor changes... Added "totalScaleFactor"

---
+++ BalloonButton/FailScreen.swift
@@ -0,0 +1,16 @@
+//
+//  FailScreen.swift
+//  HomeostasisZone
+//
+//  Created by BHATTACHARYA, ARUNIMA on 4/13/16.
+//  Copyright © 2016 BHATTACHARYA, ARUNIMA. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import UIKit
+import AVFoundation
+
+class FailSreen: UIViewController {
+    
+}
--- BalloonButton/GameScene.swift
+++ BalloonButton/GameScene.swift
@@ -23,6 +23,7 @@ class GameScene: SKScene {
     let airOutput = SKAction.scaleTo(CGFloat(0.25), duration: NSTimeInterval(30))
     var scaleFactor1 = retrieveDouble("airPump1")
     var scaleFactor2 = retrieveDouble("airPump2")
+    var totalScaleFactor = 0.0
     
     //Timer Variables
     var defaultseconds = Int(retrieveDouble("timer")!)
@@ -53,20 +54,20 @@ class GameScene: SKScene {
         myLabel.fontSize = 50
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame))
         
-        //Settings Node
-        /*mySettings.xScale = 0.50
-        mySettings.yScale = 0.50
-        mySettings.position = CGPoint(x:CGRectGetMaxX(self.frame) - 100, y:CGRectGetMaxY(self.frame) - 40)*/
-        
         //Set Up Balloon in center
         myBalloon.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        myBalloon.xScale = 1.25
-        myBalloon.yScale = 1.25
+        //Set Point for Scale = 1.65
+        //Maximum for Scale = 1.74
+        //Minimum for Scale = 1.52
+        myBalloon.xScale = 1.65
+        myBalloon.yScale = 1.65
+        //myBalloon.alpha = 0.50
         
         //Set Up Set Point
         mySetPoint.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         mySetPoint.xScale = 1.5
         mySetPoint.yScale = 1.5
+        mySetPoint.alpha = 0.50
         
         //Set Up Button1
         myButton1.position = CGPoint(x: CGRectGetMidX(self.frame) - 400, y: 150)
@@ -89,7 +90,6 @@ class GameScene: SKScene {
         self.addChild(myLabel)
         self.addChild(myTimer)
         self.addChild(myStartNode)
-        //self.addChild(mySettings)
         
     }
     
@@ -143,16 +143,36 @@ class GameScene: SKScene {
         if myButton1.containsPoint(touch.locationInNode(self)) {
             //stop constantOutput
             myBalloon.removeActionForKey("constantOutput")
+        
             let myBalloonHScale = myBalloon.xScale
             let myBalloonVScale = myBalloon.yScale
+            
+            //Check that timer is running
             if (!timerRunning) {
                 myBalloon.xScale = myBalloonHScale
                 myBalloon.yScale = myBalloonVScale
             }
             else {
+                //Check to see if within ring
+                if (myBalloonHScale > 1.74 || myBalloonHScale < 1.52) {
+                    //Stop if out of ring
+                    //self.scene?.backgroundColor = UIColor.blackColor()
+                    myTimer.text = "00:00"
+                    timer.invalidate()
+                    timerRunning = false
+                    timerCount = defaultseconds
+                    
+                    //Bring up fail screen
+                    //openViewController()
+                }
+                
+                //Scale Balloon
                 let totalScaleFactor1 = 1 + scaleFactor1!
                 myBalloon.xScale = myBalloonHScale * CGFloat(totalScaleFactor1)
                 myBalloon.yScale = myBalloonVScale * CGFloat(totalScaleFactor1)
+                totalScaleFactor = Double(myBalloon.xScale)
+                
+                //Restart constantOutput
                 constantOutput()
             }
         }
@@ -164,14 +184,34 @@ class GameScene: SKScene {
             myBalloon.removeActionForKey("constantOutput")
             let myBalloonHScale = myBalloon.xScale
             let myBalloonVScale = myBalloon.yScale
+            
+            //Check if timer is running
             if (!timerRunning) {
                 myBalloon.xScale = myBalloonHScale
                 myBalloon.yScale = myBalloonVScale
             }
             else {
+                //Check if outside of ring
+                if (myBalloonHScale > 1.74 || myBalloonHScale < 1.52) {
+                    //Stop game if outside of ring
+                    //self.scene?.backgroundColor = UIColor.blackColor()
+                    myTimer.text = "00:00"
+                    timer.invalidate()
+                    timerRunning = false
+                    timerCount = defaultseconds
+                    
+                    //Bring up fail screen
+                    //openViewController()
+
+                }
+                
+                //Scale Balloon
                 let totalScaleFactor2 = 1 + scaleFactor2!
                 myBalloon.xScale = myBalloonHScale *  CGFloat(totalScaleFactor2)
                 myBalloon.yScale = myBalloonVScale *  CGFloat(totalScaleFactor2)
+                totalScaleFactor = Double(myBalloon.xScale)
+                
+                //Restart constantOutput
                 constantOutput()
             }
         }
@@ -193,22 +233,15 @@ class GameScene: SKScene {
         
         let scaleAction = SKAction.scaleTo(0.0, duration: Double(timerCount))
         myBalloon.runAction(scaleAction, withKey: "constantOutput")
-        
-    /*
-        let myBalloonHScale = myBalloon.xScale
-        let myBalloonVScale = myBalloon.yScale
-        myBalloon.xScale = myBalloonHScale * 0.975
-        myBalloon.yScale = myBalloonVScale * 0.975
-        if (myBalloon.xScale > 0.5) {
-            constantOutput()
-        }
-        else {
-            myBalloon.xScale = 0.55
-            myBalloon.yScale = 0.55
-        }
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */*/
+        /* Called before each frame is rendered */
+    }
+    
+    func openViewController () {
+        var vc: UIViewController = UIViewController()
+        vc = self.view!.window!.rootViewController!
+        vc.performSegueWithIdentifier("show", sender: vc)
     }
 }
